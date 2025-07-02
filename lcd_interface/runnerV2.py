e88b2f0d5d579a0460d54611ee1f87db5a0e1150
Kaylie Kim <kkayliekim@gmail.com>
2025-07-02 16:06:07 -0400

    Update runnerV2.py

--- lcd_interface/runnerV2.py
+++ lcd_interface/runnerV2.py
@@ -4,22 +4,15 @@ from time import sleep_ms
 import helperFunctions
 
 def launch(display, testName, function):
-  """
-    Takes 3 parameters: 
-    1. display  
-    2. testName (name of test to be run --> from self.PCBTests or self.chipTests)
-    3. function (from helperFunctions module) 
-
-    Checks if display is connected, displays launch text on LCD & runs function 
-  """
     if display.connected:
         display.clear()
-        display.text("Launching " + testName, 80,120)
+        display.text("Launching " + testName, 160 - (len(testName)*8 + 80)//2 ,120)
 
     else: #No connection (failed to display LCD) 
         print("Launching", testName)
     sleep_ms(1000)
     
     function()
+
 
 
